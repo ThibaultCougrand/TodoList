347ee5551c5e0a726f8df798b346f32ef2af64f4
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-04-29 16:57:02 +0200

    Ajout de le gestion du temps

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    // Couleurs de post-it
     let colorsPostIt = [
         "#ebe47a",
         "#54b4c3",
@@ -7,8 +9,17 @@ $(function () {
         "#abef66"
     ];
 
+    // variable de positionnement des élément (corespond à order flex)
     let i = 100;
 
+    // date local
+    let d = new Date();
+    //let localHour = d.getHours();
+    let localDay = d.getDate();
+    let localMonth = (d.getMonth() + 1);
+    let localYear = (d.getFullYear());
+
+    // conteneur de liste
     let todo = $(".container-list");
 
     // selectionne la div cliquée
@@ -23,23 +34,38 @@ $(function () {
 
     // bouton ajouter
     $(".add-item").on("click", function () {
+
+        // choix de la couleur random des taches
         let randomColor = Math.floor(Math.random() * colorsPostIt.length);
+
+        //gestion de la date
+        let date = $(".date-list").val().toString();
+        let day = date.substring(8);
+        let month = date.substring(5, 7);
+        let year = date.substring(0, 4);
+
+        //contenu des taches
         let title = $(".title-list").val();
         let content = $(".content-list").val();
+        let hourTask = $(".time-list").val().toString().replace(":", "h");
+        let dateTask = day + "-" + month + "-" + year;
+
+        //créer la liste
         if (title !== "" && content !== "") {
             let article = $("<article></article>").addClass("item-list");
             article.css("order", i);
             article.css("background-color", colorsPostIt[randomColor]);
-            console.log(randomColor);
             let titleArticle = $("<h3></h3>").text(title);
             let containArticle = $("<p></p>").text(content);
+            let timedatetask = $('<p class="timedate"></p>').html(hourTask+" le "+dateTask);
             article.append(titleArticle);
             article.append(containArticle);
+            article.append(timedatetask);
             todo.append(article);
             i--;
         }
-        title.val("");
-        content.val("");
+        $(".title-list").val("");
+        $(".content-list").val("");
     });
 
 
@@ -60,6 +86,8 @@ $(function () {
             $(".clicked h3").text(title);
             $(".clicked p").text(content);
         }
+        $(".title-list").val("");
+        $(".content-list").val("");
     });
 
     // bouton monter
@@ -82,11 +110,32 @@ $(function () {
     // bouton de sauvegarde de list
     $(".save-item").on("click", function () {
         localStorage.setItem('todo', todo.html());
+        localStorage.setItem('i', i);
     });
 
     // chargement de la liste depuis le localStorage
     var saved = localStorage.getItem('todo');
+    var savedI = localStorage.getItem('i');
     if (saved && saved !== undefined) {
         todo.html(saved);
+        i = savedI;
     }
+
+    // vérifie les dates de fin
+    function verifDate () {
+        $(".timedate").each(function (i) {
+            //let hourTask = $(this).text().toString().substring(0, 2);
+            let dayTask = $(this).text().toString().substring(9, 11);
+            let monthTask = $(this).text().toString().substring(12, 14);
+            let yearTask = $(this).text().toString().substring(15, 19);
+            if (dayTask == localDay+1 && monthTask == localMonth && yearTask == localYear) {
+                $(this).parent().css("background-color", "#DF3434");
+            } else if (dayTask == localDay && monthTask == localMonth && yearTask == localYear) {
+                $(this).parent().addClass("end-list");
+            }
+        });
+    }
+    
+    verifDate();
+    setInterval(verifDate, 10000);
 });
